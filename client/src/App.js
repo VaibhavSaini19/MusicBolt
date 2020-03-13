8e437eaf13599e0f18ab217299d6df00fae32749
Vaibhav Saini <32795689+VaibhavSaini19@users.noreply.github.com>
2020-03-13 20:49:16 +0530

    Add Bootstrap to React

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
+				<a class="navbar-brand" href="#">
+					Navbar
+				</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse " id="navbarNav">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item mr-5">
+							<a class="nav-link" href="#">
+								Home
+							</a>
+						</li>
+						<li class="nav-item mr-5">
+							<a class="nav-link" href="#">
+								About
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">
+								Log In
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<section className="container">
+				<div className="row">
+					<div className="col-lg-6 col-md-12">asd</div>
+					<div className="col-lg-6 col-md-12">asd</div>
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
