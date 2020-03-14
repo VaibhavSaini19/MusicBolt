97a63f9499f454e56659ac7edf191ffb6d32be9a
Vaibhav Saini <32795689+VaibhavSaini19@users.noreply.github.com>
2020-03-14 21:09:50 +0530

    Edit Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 
 function App() {
 	return (
-		<div className="">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
-				<a class="navbar-brand" href="#">
+		<div className="hero-gradient">
+			<nav class="navbar navbar-expand-lg px-5">
+				<a class="navbar-brand text-white" href="#">
 					Navbar
 				</a>
 				<button
@@ -22,17 +22,17 @@ function App() {
 				<div class="collapse navbar-collapse " id="navbarNav">
 					<ul class="navbar-nav ml-auto">
 						<li class="nav-item mr-5">
-							<a class="nav-link" href="#">
+							<a class="nav-link text-white" href="#">
 								Home
 							</a>
 						</li>
 						<li class="nav-item mr-5">
-							<a class="nav-link" href="#">
+							<a class="nav-link text-white" href="#">
 								About
 							</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#">
+							<a class="nav-link text-white" href="#">
 								Log In
 							</a>
 						</li>
@@ -40,9 +40,20 @@ function App() {
 				</div>
 			</nav>
 			<section className="container">
-				<div className="row">
-					<div className="col-lg-6 col-md-12">asd</div>
-					<div className="col-lg-6 col-md-12">asd</div>
+				<div className="row py-5">
+					<div className="col-lg-6 col-md-12 text-white">
+						<div className="row tagline">Music, that touches your soul</div>
+						<div className="row tagline-helper my-5">Anywhere. Everywhere</div>
+						<div className="row cta-grp">
+							<div className="col cta">Get Started</div>
+							<div className="col cta">Learn more</div>
+						</div>
+					</div>
+					<div className="col-lg-6 col-md-12 py-5">
+						{/* <img src="./img/music-bg-1.jpg" className="img-fluid"></img> */}
+						{/* <img src="./img/music-person-1.jpg" className="img-fluid"></img> */}
+						<img src="./img/music-person-1-nobg.png" className="img-fluid"></img>
+					</div>
 				</div>
 			</section>
 		</div>
