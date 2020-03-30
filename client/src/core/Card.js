4941b14e68b075a5b3f452840a79cbb1ed6e3f78
Vaibhav Saini <32795689+VaibhavSaini19@users.noreply.github.com>
2020-03-30 20:40:35 +0530

    Add Rating UI for tracks

--- client/src/core/Card.js
+++ client/src/core/Card.js
@@ -13,6 +13,20 @@ const Card = ({ track }) => {
 				<div className="trackCard-name">{name}</div>
 				<div className="trackCard-artist">{artist}</div>
 			</div>
+			<div className="trackCard-rating">
+				<span className="starRating">
+					<input id={`${id}rating5`} type="radio" name={`${id}rating`} value="5" />
+					<label htmlFor={`${id}rating5`}>5</label>
+					<input id={`${id}rating4`} type="radio" name={`${id}rating`} value="4" />
+					<label htmlFor={`${id}rating4`}>4</label>
+					<input id={`${id}rating3`} type="radio" name={`${id}rating`} value="3" />
+					<label htmlFor={`${id}rating3`}>3</label>
+					<input id={`${id}rating2`} type="radio" name={`${id}rating`} value="2" />
+					<label htmlFor={`${id}rating2`}>2</label>
+					<input id={`${id}rating1`} type="radio" name={`${id}rating`} value="1" />
+					<label htmlFor={`${id}rating1`}>1</label>
+				</span>
+			</div>
 		</div>
 	);
 };
--- client/src/user/UserDashboard.js
+++ client/src/user/UserDashboard.js
@@ -42,35 +42,47 @@ const UserDashboard = () => {
 	return (
 		<Base>
 			<section className="container mt-5" id="songsSection">
-				<h1 className="font-weight-bold">Tracks</h1>
-				<div className="tracks-area">
-					<div className="button-group">
-						<button type="button" data-filter="*" className="active" id="btn1">
-							All
-						</button>
-						<button type="button" data-filter=".popular">
-							Popular
-						</button>
-						<button type="button" data-filter=".latest">
-							Latest
-						</button>
-						<button type="button" data-filter=".upcoming">
-							Upcoming
-						</button>
+				<nav>
+					<div className="nav nav-fill nav-lg nav-tabs" id="nav-tab" role="tablist">
+						<a className="nav-item nav-link active" id="nav-tracks-tab" data-toggle="tab" href="#nav-tracks" role="tab" aria-controls="nav-tracks" aria-selected="true"><h3>Tracks</h3></a>
+						<a className="nav-item nav-link" id="nav-fav-tab" data-toggle="tab" href="#nav-fav" role="tab" aria-controls="nav-fav" aria-selected="false"><h3>Favourites</h3></a>
 					</div>
-					<div className="row grid mt-5">
-						{tracks && tracks.map((track, index) => {
-							return (
-								<div
-									key={index}
-									className={`col-lg-4 col-md-6 col-sm-12 element-item track mb-4 ${
-										trackTypes[Math.floor(Math.random() * trackTypes.length)]
-									}`}
-								>
-									<Card track={track} />
-								</div>
-							);
-						})}
+				</nav>
+				<div className="tab-content" id="nav-tabContent">
+					<div className="tab-pane fade show active" id="nav-tracks" role="tabpanel" aria-labelledby="nav-home-tab">
+						<div className="tracks-area">
+							<div className="button-group">
+								<button type="button" data-filter="*" className="active" id="btn1">
+									All
+								</button>
+								<button type="button" data-filter=".popular">
+									Popular
+								</button>
+								<button type="button" data-filter=".latest">
+									Latest
+								</button>
+								<button type="button" data-filter=".upcoming">
+									Upcoming
+								</button>
+							</div>
+							<div className="row grid mt-5">
+								{tracks && tracks.map((track, index) => {
+									return (
+										<div
+											key={index}
+											className={`col-lg-4 col-md-6 col-sm-12 element-item track mb-4 ${
+												trackTypes[Math.floor(Math.random() * trackTypes.length)]
+											}`}
+										>
+											<Card track={track} />
+										</div>
+									);
+								})}
+							</div>
+						</div>
+					</div>
+					<div className="tab-pane fade" id="nav-fav" role="tabpanel" aria-labelledby="nav-fav-tab">
+
 					</div>
 				</div>
 			</section>
